8b99eaed8e729fc2a41ff586e562b5efdcbd4c89
Jakub Ziemiański <jakubziemianski@air-jakub.home>
2024-02-27 17:55:18 +0100

    zmieniono strone wizualna i responsywnosc

--- src/js/aos.js
+++ src/js/aos.js
@@ -1,8 +1,6 @@
 AOS.init({
-    startEvent: 'DOMContentLoaded',
-    offset: 200,
-    once: true,
-    duration: 600,
-    anchorPlacement: 'top-center',
-
-});
+	startEvent: 'DOMContentLoaded',
+	offset: 200,
+	once: true,
+	duration: 600,
+})
